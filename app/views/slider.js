5955baad7640644fe8db48e6a56dbda12d3b557a
Matthijs Kuiper <info@matthijskuiper.nl>
2014-10-03 18:15:07 +0200

    add markers to slider

--- app/views/slider.js
+++ app/views/slider.js
@@ -3,17 +3,20 @@ import Ember from "ember";
 export default Ember.View.extend({
   classNames: ['slider'],
   templateName: 'slider',
+  startValue: 0,
 
   createSlider: function() {
+    var self = this;
+
     // init noUiSlider
     this.$('.slider-ui').noUiSlider({
-      start: 50,
+      start: self.get('startValue'),
       orientation: 'vertical',
+      direction: 'rtl',
       range: {
         'min': 0,
         'max': 100
       }
     });
-
   }.on('didInsertElement')
 });
