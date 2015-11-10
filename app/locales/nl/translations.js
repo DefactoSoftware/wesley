fdaafa5b0a9bcef97c8cb670c2b51c9f15ba5725
Hugo Veeger <hugo@learningspaces.io>
2015-11-10 16:31:57 +0100

    add new english translations

--- app/locales/nl/translations.js
+++ app/locales/nl/translations.js
@@ -4,7 +4,7 @@ export default {
     'video': 'Video',
     'instructions': 'Instructies',
     'prom': 'PROM studie',
-    'opt': 'OPT studie'
+    'opt': 'OPTion studie'
   },
   'sliders': {
     'alive': 'Levensverlenging',
@@ -21,5 +21,25 @@ export default {
   'video': {
     'introduction': 'In het onderstaande filmpje ziet u een voorbeeld van een gesprek waarin de OPT wordt gebruikt. In dit voorbeeld wordt de OPT toegepast bij een oudere patiënt die veel verschillende medicijnen gebruikt.',
     'url': 'https://www.youtube.com/embed/GEOrnoU5ydg'
+  },
+  'opt': {
+    'p1': 'De OPT (outcome prioritization tool) is een gesprekshulp om behandelvoorkeuren duidelijk te maken. Het is een kaart met vier visueel analoge schalen die elk een behandeldoel weergeven:',
+    'ul1': '<li>Levensverlenging</li><li>Onafhankelijkheid bewaren</li><li>Verminderen of wegnemen van pijn</li><li>Verminderen of wegnemen van andere symptomen</li>',
+    'p2': 'Door een plaats op deze schaal te kiezen drukt de patiënt zijn waardering uit voor het betreffende doel en brengt een rangorde aan in de doelen.',
+    'p3': 'De OPT is ontwikkeld in de Verenigde Staten in 2011<sup>1</sup> en daarna getest in Nederland.<sup>2</sup> De OPT kan door zorgverleners worden gebruikt voor verschillende patiëntengroepen en in verschillende gesprekssituaties.',
+    'references': 'References',
+    'ol1': '<li>Fried,T.R.; Tinetti,M.; Agostini,J.; Iannone,L.; Towle,V. Health outcome prioritization to elicit preferences of older persons with multiple health conditions. Patient Educ Couns, 2011, 83, 2, 278-282</li><li>Schuling, J; Sytema, R; Berendsen, AJ. Adjusting medication: elderly patient\'s preference counts. Ned Tijdschr Geneeskd, 2013, 157, 47, A6491</li>'
+  },
+  'prom': {
+    'title': 'Preferenties van ouderen met multimorbiditeit (PROM)',
+    'p1': 'De afdeling huisartsgeneeskunde van het UMCG doet onderzoek naar het gebruik van de OPT. De PROM studie werd uitgevoerd van oktober 2013 t/m april 2014. Bij patiënten ouder dan 80 jaar die meer dan vijf verschillende medicijnen gebruiken voerde de huisarts een gesprek met hulp van de OPT. Zonodig werd de medicatie aangepast.',
+    'li1': '<li><a href="assets/13-NTvG-medicatie-voorkeuren.pdf">Download het artikel</a></li><li><a href="assets/Poster.ppt">Download de poster</a></li>'
+  },
+  'option': {
+    'p1': 'De afdeling huisartsgeneeskunde van het UMCG doet onderzoek naar het gebruik van de OPT. In oktober 2015 start de OPTion-studie. Oudere patiënten (≥ 70 jaar) uit ziekenhuizen in het noorden van Nederland die net te horen hebben gekregen dat ze (waarschijnlijk) een ongeneeslijke kanker hebben, kunnen meedoen. Ze worden door loting in twee groepen verdeeld. De ene groep ontvangt zorg zoals altijd, de andere groep heeft gesprekken met de huisarts waarin de OPT wordt gebruikt.<ul><li><a href="assets/option-algemene-specialisteninformatie.pdf" target="_blank">Toelichting voor specialisten</a></li><li><a href="assets/option-algemene-huisartseninformatie.pdf" target="_blank">Informatie voor huisartsen</a></li><li><a href="assets/option-informatie-patienten.pdf" target="_blank">Informatie voor patiënten</a></li><li><a {{action \'openModal\' \'example-option\'}}>Uitgebreid voorbeeld OPTion</a></li></ul>',
+    'team': 'Het OPTion projectteam bestaat uit:',
+    'ul1': '<li>Ms. Dr A.J. (Annette) Berendsen, huisarts en projectleider</li><li>Mr. Dr J. Schuling (Jan), oud-huisarts en onderzoeker</li><li>Ms. Drs. M.E. (Mariken) Stegmann, aios huisartsgeneeskunde en promovenda</li><li>Ms. Prof. Dr. M.Y. (Marjolein) Berger, huisarts en hoofd afdeling</li>',
+    'contact': 'Contactinformatie',
+    'ul2': '<li>Tel: <a href="tel:06 155 25 388">06 155 25 388</a></li><li>Fax: 050 363 2964 (t.n.v. OPTion)</li><li><a href="mailto:option-studie@umcg.nl">option-studie@umcg.nl</a></li>'
   }
 };
